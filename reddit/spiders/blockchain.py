204e1a5d389aa3f6f6f5080c0f989208f8dc7f6d
Gerard Moroney <gerard.x.moroney@gmail.com>
2017-09-03 19:21:22 +0100

    Changed xpath setting to correct how scrapy parses postings

--- reddit/spiders/blockchain.py
+++ reddit/spiders/blockchain.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Request
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 class BlockchainSpider(scrapy.Spider):
+
     name = 'blockchain'
     allowed_domains = ['reddit.com']
     start_urls = ['https://www.reddit.com/r/BlockChain/']
 
     def parse(self, response):
 
-        postings = response.xpath('//p[@class="title"]')
+        postings = response.xpath('//div[@class="top-matter"]')
 
         for record in postings:
-            docTitle = record.xpath('a/text()').extract_first()
-            docUrl = record.xpath('a/@href').extract_first()
+            docTitle = record.xpath('p[@class="title"]/a/text()').extract_first()
+            docUrl = record.xpath('p[@class="title"]/a/@href').extract_first()
             docUrl = response.urljoin(docUrl)
             yield { 'Title': docTitle, 'URL': docUrl }
-            relative_next_url = response.xpath('//a[contains(text(),"next" )]/@href').extract_first()
+            relative_next_url = response.xpath('//span[@class="next-button"]/a/@href').extract_first()
             absolute_next_url = response.urljoin(relative_next_url)
             yield Request(absolute_next_url, callback=self.parse)
