9f4f768d71ae6bdd0e6a2793d1ff07d2665099aa
Gerard Moroney <gerard.x.moroney@gmail.com>
2017-09-03 08:31:09 +0100

    Basic scraping without database storage; stops at 50 entries. Have tried to set values for delay and auto-throttle but no effect

--- reddit/spiders/blockchain.py
+++ reddit/spiders/blockchain.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy import Request
 
 class BlockchainSpider(scrapy.Spider):
     name = 'blockchain'
     allowed_domains = ['reddit.com']
-    start_urls = ['https://www.reddit.com/r/BlockChain//']
+    start_urls = ['https://www.reddit.com/r/BlockChain/']
 
     def parse(self, response):
-        pass
+
+        postings = response.xpath('//p[@class="title"]')
+
+        for record in postings:
+            docTitle = record.xpath('a/text()').extract_first()
+            docUrl = record.xpath('a/@href').extract_first()
+            docUrl = response.urljoin(docUrl)
+            yield { 'Title': docTitle, 'URL': docUrl }
+            relative_next_url = response.xpath('//a[@class="next-button"]/@href').extract_first()
+            absolute_next_url = response.urljoin(relative_next_url)
+            yield Request(absolute_next_url, callback=self.parse)
