48fa9a9027d97553090d23ad195567cc91ac6aec
Gerard Moroney <gerard.x.moroney@gmail.com>
2017-09-04 02:40:42 +0100

    Added document author and date details to scrape

--- reddit/spiders/blockchain.py
+++ reddit/spiders/blockchain.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy import Request
+#from reddit.items import RedditItem
 
 class BlockchainSpider(scrapy.Spider):
 
@@ -13,10 +14,26 @@ class BlockchainSpider(scrapy.Spider):
         postings = response.xpath('//div[@class="top-matter"]')
 
         for record in postings:
+
+            # Create item object to capture data
+            #item = RedditItem()
+
             docTitle = record.xpath('p[@class="title"]/a/text()').extract_first()
             docUrl = record.xpath('p[@class="title"]/a/@href').extract_first()
             docUrl = response.urljoin(docUrl)
-            yield { 'Title': docTitle, 'URL': docUrl }
+            docAuthor = record.xpath('p[@class="tagline "]/a/text()').extract_first()
+            docAuthorUrl = record.xpath('p[@class="tagline "]/a/@href').extract_first()
+            docTimestamp = record.xpath('p[@class="tagline "]/time/@datetime').extract_first()[:10]
+            yield { 'Title': docTitle, 'URL': docUrl, 'Author': docAuthor, 'AuthorURL': docAuthorUrl, 'DateCreated': docTimestamp }
+
+            # Call the function to get the next page
             relative_next_url = response.xpath('//span[@class="next-button"]/a/@href').extract_first()
+
+            # Get the URL for the next page
             absolute_next_url = response.urljoin(relative_next_url)
+
+            # Recursively call the parse function to get content from the next page
             yield Request(absolute_next_url, callback=self.parse)
+
+~                                                                                                                                                      
+~
